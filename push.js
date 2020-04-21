7ed77e5aa37e82f96193708a0b57d27bf7ac054d
Farkash <pini@emea.teo.earth>
2020-04-21 16:21:01 +0300

    initial

---
+++ push.js
@@ -0,0 +1,11 @@
+var push = require('web-push')
+
+let vapidKeys = {
+    publicKey: 'BF21kD_nyZBb0XifN6_SPTaDVRaPNkPyWXQUaGw0U5UKlmYQCIzLO8gIuL-5IWk5BoecpSv9xiEsm9ZOGev6iIQ',
+    privateKey: 'AM0yd7Fz9EMN5JrY-MvEYL6q9gqdR87mVUGNw7p17ZY'
+  }
+
+push.setVapidDetails('mailto:test@example.com', vapidKeys.publicKey, vapidKeys.privateKey);
+
+let sub = {};
+push.sendNotification(sub, 'test message');
