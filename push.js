a8dacd7dc5bda21ae01d3af159d56cc26e6bd955
Farkash <pini@emea.teo.earth>
2020-04-21 18:10:09 +0300

    fix

--- push.js
+++ push.js
@@ -7,5 +7,6 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@example.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/flDSa_J35i8:APA91bF6jSniecKC_7od2AHIs4Tp1f3rwQ3isoHv_E6D8mZSEGqgJE_NpAWZd4dAOVgkvn5dW7-6U6uf4GDHD2ng_OhSJJdpFcisDOlKtoRNj_v4W1oymythDLdJQWQirhMGlDDNX5LC","expirationTime":null,"keys":{"p256dh":"BLaNOUTu97izhK7ErYLWySK42KaYLz3ZPmf9rfL3bxf44sr7dQY04Yf7iuVEMAXED7KYPl1QnvLo5wRdXGPU53o","auth":"69ZwNky-CB6meUqM8TFmwQ"}};
+
 push.sendNotification(sub, 'test message');
